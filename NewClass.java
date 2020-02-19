02bb43bee0d51723284860f976da11d89c95b954
Xhoni792 <61250993+Xhoni792@users.noreply.github.com>
2020-02-19 21:34:51 +0100

    Add files via upload
    That's a simple Real Estate program with Java. This program uses MYSQL database.

---
+++ NewClass.java
@@ -0,0 +1,9 @@
+
+package project1;
+
+public class NewClass {
+  public static void main(){
+      Login log = new Login();
+      log.setVisible(true);
+  }  
+}
---
+++ db_connect.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+
+public class db_connect {
+public static Connection db_connection(){
+    Connection con=null;
+    try{
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      con =(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3308/compputer_application_project?allowPublicKeyRetrieval useSSL=FALSE;","root","Aria2013");
+    }
+    catch(Exception e){
+        JOptionPane.showMessageDialog(null, "not connnected with dtabase");
+        System.out.println(e);
+    }
+    return con;
+}  //end of method  
+    
+}//end of class
+
+
+
